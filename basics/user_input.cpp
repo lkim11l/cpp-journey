9d09c60d6ab276797ca4bddd4f428f3f0fbfa1e5
lkim11l <kimr48899@gmail.com>
2026-03-21 22:50:04 -0700

    Add user input program with name and age
    Program that takes user input (name and age) and displays formatted output.

---
+++ basics/user_input.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  string name;
+  string major;
+  int age;
+
+  cout <<"Enter your name: ";
+  cin >> name;
+  cout <<"Enter your major: ";
+  cin >> major;
+  cout <<"Enter your age: ";
+  cin >> age;
+
+  cout <<"My name is "<<name<<" !"<< endl;
+  cout <<"My major is "<<major<< endl;
+  cout <<"I am "<<age<<" years old"<< endl;
+
+  return 0;
+    }
+
+
